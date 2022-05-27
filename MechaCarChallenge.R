58d18b77e9f94d43754f8a01a9b45ca058dadcd3
Sophie Hearn <sophieehearn@gmail.com>
2022-05-26 18:19:35 -0700

    update code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,14 @@
 library(tidyverse)
 setwd("C:/Users/sophi/OneDrive/Desktop/GitHubRepo/MechaCar_Statistical_Analysis")
-Mecha_Car <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_Car) 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_Car))
 
+suspensionCoils <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+totalSummary <- suspensionCoils %>%  summarize(Mean=mean(PSI),Median=median(PSI),Variance = var(PSI), SD = sd(PSI),  .groups = 'keep')
+lotSummary <- suspensionCoils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Medain=median(PSI),Variance = var(PSI), SD = sd(PSI),  .groups = 'keep')
+
+t.test(suspensionCoils$PSI, mu = 1500)
+t.test(subset(suspensionCoils,Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(suspensionCoils,Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(suspensionCoils,Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
